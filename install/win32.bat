a52a55b37f6a888cfb2af3980d07e4881136d7a5
damu <damu@pinduoduo.com>
2017-12-24 16:17:09 +0800

    fix vim-plug run path

--- install/win32.bat
+++ install/win32.bat
@@ -2,8 +2,9 @@ echo "[lin-vim] Install for Windows"
 
 rem Vim Plugins
 mkdir %HOMEPATH%\.ssh
-curl -fLo %HOMEPATH%\.vim\autoload\plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir %HOMEPATH%\vimfiles
+curl -fLo %HOMEPATH%\vimfiles\autoload\plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp %HOMEPATH%\.vim\lin-vim.vimrc %HOMEPATH%\_vimrc
-vim -c "source ~/.vim/settings/vim-plug.vim" -c "PlugInstall" -c "qall"
+vim -c "PlugInstall" -c "qall"
 cd %HOMEPATH%\.vim\plugged\YouCompleteMe
-python install.py --clang-completer --tern-completer
+python install.py --clang-completer --js-completer
