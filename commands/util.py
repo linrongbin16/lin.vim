e9450679111581d96b636c59647929645b4cbba3
linrongbin <linrongbin16@gmail.com>
2017-12-30 09:41:39 +0800

    update commands

--- commands/util.py
+++ commands/util.py
@@ -1,28 +1,33 @@
 #! /usr/bin/env python
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
+'''common utils for lin-vim commands'''
 
 import sys
-sys.path.append('.')
 import os
 import traceback
 import types
+sys.path.append('.')
+
+
+ag_ignore = '~/.vim/commands/ag.ignore'
 
 
 def command_name():
-    s = sys.argv[0]
-    if s.rfind('/') >= 0:
-        s = s[s.rfind('/') + 1:]
-    elif s.rfind('\\') >= 0:
-        s = s[s.rfind('\\') + 1:]
-    if s.find('.') >= 0:
-        s = s[:s.find('.')]
-    return s.strip()
+    '''get command name'''
+    name = sys.argv[0]
+    if name.rfind('/') >= 0:
+        name = name[name.rfind('/') + 1:]
+    elif name.rfind('\\') >= 0:
+        name = name[name.rfind('\\') + 1:]
+    if name.find('.') >= 0:
+        name = name[:name.find('.')]
+    return name.strip()
 
 
 def helpmsg(briefs, usages):
     """print command message"""
-    assert isinstance(briefs, list) or isinstance(briefs, str)
-    assert isinstance(usages, list) or isinstance(usages, str)
+    assert isinstance(briefs, (list, str))
+    assert isinstance(usages, (list, str))
     assert type(briefs) == type(usages)
     if isinstance(briefs, list):
         assert len(briefs) == len(usages)
@@ -74,8 +79,9 @@ def makesure(msg):
 
 
 def execute(cmd):
+    '''execute shell command'''
     tmp = ".util.%d.tmp" % os.getpid()
-    assert isinstance(cmd, str) or isinstance(cmd, list)
+    assert isinstance(cmd, (list, str))
     if isinstance(cmd, str):
         os.system("%s > %s" % (cmd, tmp))
     else:
@@ -84,17 +90,18 @@ def execute(cmd):
             cmds = cmds + i + " "
         os.system("%s > %s" % (cmds, tmp))
     fp = open(tmp, "r")
-    output = ""
+    retval = ""
     for line in fp.readlines():
-        output = output + line
+        retval = retval + line
     fp.close()
     os.system("rm %s" % tmp)
-    return output.strip()
+    return retval.strip()
 
 
 def run(cmd):
+    '''run shell command and return the retval'''
     tmp = ".util.%d.tmp" % os.getpid()
-    assert isinstance(cmd, str) or isinstance(cmd, list)
+    assert isinstance(cmd, (list, str))
     if isinstance(cmd, str):
         os.system("%s > %s" % (cmd, tmp))
     else:
@@ -103,45 +110,9 @@ def run(cmd):
             cmds = cmds + i + " "
         os.system("%s > %s" % (cmds, tmp))
     fp = open(tmp, "r")
-    output = ""
+    retval = ""
     for line in fp.readlines():
-        output = output + line
+        retval = retval + line
     fp.close()
     os.system("rm %s" % tmp)
-    return output.strip()
-
-
-"""
-def run_as_admin_for_win(cmdLine):
-    assert os.name == 'nt'
-    # import win32api
-    import win32con
-    import win32event
-    import win32process
-    from win32com.shell.shell import ShellExecuteEx
-    from win32com.shell import shellcon
-    assert isinstance(cmdLine, list)
-    cmd = '"%s"' % cmdLine[0]
-    # TODO: isn't there a function or something we can call to massage command line params?
-    params = " ".join(['"%s"' % (x,) for x in cmdLine[1:]])
-    showCmd = win32con.SW_SHOWNORMAL
-    lpVerb = 'runas'  # causes UAC elevation prompt.
-
-    print("Running cmd: %s, params: %s" % (cmd, params))
-
-    # ShellExecute() doesn't seem to allow us to fetch the PID or handle
-    # of the process, so we can't get anything useful from it. Therefore
-    # the more complex ShellExecuteEx() must be used.
-
-    # procHandle = win32api.ShellExecute(0, lpVerb, cmd, params, cmdDir, showCmd)
-
-    procInfo = ShellExecuteEx(nShow=showCmd,
-                              fMask=shellcon.SEE_MASK_NOCLOSEPROCESS,
-                              lpVerb=lpVerb,
-                              lpFile=cmd,
-                              lpParameters=params)
-    procHandle = procInfo['hProcess']
-    obj = win32event.WaitForSingleObject(procHandle, win32event.INFINITE)
-    rc = win32process.GetExitCodeProcess(procHandle)
-    return rc
-"""
+    return retval.strip()
--- lin-vim.vimrc
+++ lin-vim.vimrc
@@ -4,7 +4,6 @@ source $HOME/.vim/settings/vim-plug.vim
 source $HOME/.vim/settings/DoxygenToolkit.vim
 source $HOME/.vim/settings/ag.vim
 source $HOME/.vim/settings/ale.vim
-source $HOME/.vim/settings/clang_complete.vim
 source $HOME/.vim/settings/close_hidden_buffer.vim
 source $HOME/.vim/settings/color_theme.vim
 source $HOME/.vim/settings/cscope.vim
@@ -13,12 +12,17 @@ source $HOME/.vim/settings/ctags.vim
 source $HOME/.vim/settings/emmet.vim
 source $HOME/.vim/settings/haskell-vim.vim
 source $HOME/.vim/settings/indentline.vim
-source $HOME/.vim/settings/jedi-vim.vim
-source $HOME/.vim/settings/neocomplete.vim
+if has('win32') || matchstr(system('uname -a'), 'Microsoft') != '' || matchstr(system('uname -a'), 'Windows') != ''
+    source $HOME/.vim/settings/jedi-vim.vim
+    source $HOME/.vim/settings/neocomplete.vim
+    source $HOME/.vim/settings/clang_complete.vim
+    source $HOME/.vim/settings/supertab.vim
+else
+    source $HOME/.vim/settings/youcompleteme.vim
+endif
 source $HOME/.vim/settings/nerdtree.vim
 source $HOME/.vim/settings/python-syntax.vim
 source $HOME/.vim/settings/racer.vim
-source $HOME/.vim/settings/supertab.vim
 "source $HOME/.vim/settings/tagbar.vim
 source $HOME/.vim/settings/ultisnips.vim
 source $HOME/.vim/settings/vim-airline.vim
@@ -31,7 +35,6 @@ source $HOME/.vim/settings/vim-javacomplete2.vim
 source $HOME/.vim/settings/vim-markdown.vim
 source $HOME/.vim/settings/vimfiler.vim
 source $HOME/.vim/settings/yank.vim
-source $HOME/.vim/settings/youcompleteme.vim
 
 source $HOME/.vim/settings/basic.vim
 source $HOME/.vim/settings/indent.vim
