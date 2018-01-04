6f858eb5dfd41009d4fa1368d25812c021bb3a0f
linrongbin16 <linrongbin16@gmail.com>
2018-01-04 17:07:13 +0800

    add scripts for windows cmd

---
+++ commands/agcount.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set cmdName=%0
+set argCount=0
+for %%x in (%*) do set /A argCount+=1
+
+rem error 1: miss repository parameter
+if %argCount% LSS 1 (
+    echo Brief:
+    echo     count [text] of current directory recursively
+    echo Usage:
+    echo     %cmdName% [text]
+    echo Try again
+    echo.
+    exit /B 1
+)
+
+set text=%*
+set ignore=%HOMEPATH%\.vim\commands\ag.ignore
+
+ag -c --smart-case --depth -1 -p $ignore %text% .
