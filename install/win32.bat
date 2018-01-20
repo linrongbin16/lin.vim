e33d8cbf935accd22a2a9132ed93b84aa1fa53ed
linrongbin16 <linrongbin16@gmail.com>
2018-01-20 18:57:16 +0800

    fix windows installation

--- install/win32.bat
+++ install/win32.bat
@@ -1,15 +1,12 @@
 @ECHO OFF
 ECHO [lin-vim] Install for Windows
 
-pip install pyOpenSSL pyflakes pep8 flake8 pylint cpplint pyOpenSSL autopep8
-npm install -g js-beautify standard eslint xo typescript-formatter sass remark-cli
-
 mkdir %HOMEPATH%\.ssh
-mkdir %HOMEPATH%\vimfiles
 curl -fLo %HOMEPATH%\.vim\autoload\plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp %HOMEPATH%\.vim\lin-vim.vimrc %HOMEPATH%\_vimrc
-vim -c "PlugInstall" -c "qall"
+gvim -c "PlugInstall" -c "qall"
 cd %HOMEPATH%\.vim\plugged\YouCompleteMe
 python install.py --clang-completer --go-completer --js-completer
 
-mkdir -p %HOMEPATH%\.ssh
+pip install pyOpenSSL pyflakes pep8 flake8 pylint cpplint pyOpenSSL autopep8
+npm install -g js-beautify standard eslint xo typescript-formatter sass remark-cli
