5131ca61ab1b85ff158aebfd7749d0516cc57ce9
linrongbin16 <linrongbin16@gmail.com>
2018-03-15 19:17:17 +0800

    fix format

--- commands/gdiscard.py
+++ commands/gdiscard.py
@@ -33,18 +33,12 @@ ignore_pattern = re.compile(r'lin-vim.tmp_out.pid-[0-9]+.tid-[0-9]+')
 for i in modifies:
     if ignore_pattern.match(i):
         continue
-    if util.is_windows():
-        print('[lin-vim] discard: %s' % i)
-    else:
-        sys.stdout.write('[lin-vim] discard: %s' % i)
+    sys.stdout.write('[lin-vim] discard: %s' % i)
     os.system('git checkout %s' % i)
 for i in untracts:
     if ignore_pattern.match(i):
         continue
-    if util.is_windows():
-        print('[lin-vim] remove: %s' % i)
-    else:
-        sys.stdout.write('[lin-vim] remove: %s' % i)
+    sys.stdout.write('[lin-vim] remove: %s' % i)
     os.system('rm %s' % i)
 
 if os.path.exists(save_dir):
