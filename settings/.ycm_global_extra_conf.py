86aa0a11e9a2cb58c88ae37dcde805db960c8c47
linrongbin16 <linrongbin16@gmail.com>
2018-08-28 03:32:38 +0000

    fix ycm_extra_conf

--- settings/.ycm_global_extra_conf.py
+++ settings/.ycm_global_extra_conf.py
@@ -37,116 +37,116 @@ import ycm_core
 # compilation database set (by default, one is not set).
 # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
-'-Wall',
-'-Wextra',
-'-Werror',
-'-Wno-long-long',
-'-Wno-variadic-macros',
-'-fexceptions',
-'-DNDEBUG',
-# You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
-# source code needs it.
-'-DUSE_CLANG_COMPLETER',
-# THIS IS IMPORTANT! Without the '-x' flag, Clang won't know which language to
-# use when compiling headers. So it will guess. Badly. So C++ headers will be
-# compiled as C headers. You don't want that so ALWAYS specify the '-x' flag.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c++',
-'-isystem',
-'../BoostParts',
-'-isystem',
-get_python_inc(),
-'-isystem',
-'../llvm/include',
-'-isystem',
-'../llvm/tools/clang/include',
-'-I',
-'.',
-'-I',
-'./ClangCompleter',
-'-isystem',
-'./tests/gmock/gtest',
-'-isystem',
-'./tests/gmock/gtest/include',
-'-isystem',
-'./tests/gmock',
-'-isystem',
-'./tests/gmock/include',
-'-isystem',
-'./benchmarks/benchmark/include',
+    '-Wall',
+    '-Wextra',
+    '-Werror',
+    '-Wno-long-long',
+    '-Wno-variadic-macros',
+    '-fexceptions',
+    '-DNDEBUG',
+    # You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
+    # source code needs it.
+    '-DUSE_CLANG_COMPLETER',
+    # THIS IS IMPORTANT! Without the '-x' flag, Clang won't know which language to
+    # use when compiling headers. So it will guess. Badly. So C++ headers will be
+    # compiled as C headers. You don't want that so ALWAYS specify the '-x' flag.
+    # For a C project, you would set this to 'c' instead of 'c++'.
+    '-x',
+    'c++',
+    '-isystem',
+    '../BoostParts',
+    '-isystem',
+    get_python_inc(),
+    '-isystem',
+    '../llvm/include',
+    '-isystem',
+    '../llvm/tools/clang/include',
+    '-I',
+    '.',
+    '-I',
+    './ClangCompleter',
+    '-isystem',
+    './tests/gmock/gtest',
+    '-isystem',
+    './tests/gmock/gtest/include',
+    '-isystem',
+    './tests/gmock',
+    '-isystem',
+    './tests/gmock/include',
+    '-isystem',
+    './benchmarks/benchmark/include',
 ]
 
 # Windows MinGW header
 windows_header = [
-'-I',
-'C:\\Program Files\\mingw-w64\\x86_64-7.2.0-posix-seh-rt_v5-rev1\\mingw64\\lib\\gcc\\x86_64-w64-mingw32\\7.2.0\\include',
-'-I',
-'C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Tools\\MSVC\\14.12.25827\\include',
-'-I',
-'C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.16299.0\\ucrt',
+    '-I',
+    'C:\\Program Files\\mingw-w64\\x86_64-7.2.0-posix-seh-rt_v5-rev1\\mingw64\\lib\\gcc\\x86_64-w64-mingw32\\7.2.0\\include',
+    '-I',
+    'C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Tools\\MSVC\\14.12.25827\\include',
+    '-I',
+    'C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.16299.0\\ucrt',
 ]
 
 # Linux, Unix header
 linux_header = [
-'-I',
-'/usr/include',
-'-I',
-'/usr/lib',
-'-I',
-'/usr/include/c++',
-'-I',
-'/usr/include/c++/2',
-'-I',
-'/usr/include/c++/3',
-'-I',
-'/usr/include/c++/4',
-'-I',
-'/usr/include/c++/5',
-'-I',
-'/usr/include/c++/6'
-'-I',
-'/usr/include/c++/7'
-'-I',
-'/usr/include/c++/8'
-'-I',
-'/usr/include/c++/9'
-'-I',
-'/usr/include/x86_64-linux-gnu',
+    '-I',
+    '/usr/include',
+    '-I',
+    '/usr/lib',
+    '-I',
+    '/usr/include/c++',
+    '-I',
+    '/usr/include/c++/2',
+    '-I',
+    '/usr/include/c++/3',
+    '-I',
+    '/usr/include/c++/4',
+    '-I',
+    '/usr/include/c++/5',
+    '-I',
+    '/usr/include/c++/6',
+    '-I',
+    '/usr/include/c++/7',
+    '-I',
+    '/usr/include/c++/8',
+    '-I',
+    '/usr/include/c++/9',
+    '-I',
+    '/usr/include/x86_64-linux-gnu',
 ]
 
 # User header
 user_header = [
-'-I',
-'./src',
-'-I',
-'./include',
-'-I',
-'../include',
-'-I',
-'../../include',
-'-I',
-'../../../include',
-'-I',
-'../../../../include',
-'-I',
-'../../../../../include',
-'-I',
-'../../../../../../include',
-'-I',
-'.',
-'-I',
-'..',
-'-I',
-'../..',
-'-I',
-'../../..',
-'-I',
-'../../../..',
-'-I',
-'../../../../..',
-'-I',
-'../../../../../..',
+    '-I',
+    './src',
+    '-I',
+    './include',
+    '-I',
+    '../include',
+    '-I',
+    '../../include',
+    '-I',
+    '../../../include',
+    '-I',
+    '../../../../include',
+    '-I',
+    '../../../../../include',
+    '-I',
+    '../../../../../../include',
+    '-I',
+    '.',
+    '-I',
+    '..',
+    '-I',
+    '../..',
+    '-I',
+    '../../..',
+    '-I',
+    '../../../..',
+    '-I',
+    '../../../../..',
+    '-I',
+    '../../../../../..',
 ]
 
 if platform.system() == 'Windows':
@@ -159,7 +159,7 @@ flags.extend(user_header)
 # which is required for compiling the standard library, and to 'c++11' for older
 # versions.
 if platform.system() != 'Windows':
-  flags.append( '-std=c++11' )
+    flags.append('-std=c++11')
 
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
@@ -174,64 +174,65 @@ if platform.system() != 'Windows':
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
 compilation_database_folder = ''
 
-if os.path.exists( compilation_database_folder ):
-  database = ycm_core.CompilationDatabase( compilation_database_folder )
+if os.path.exists(compilation_database_folder):
+    database = ycm_core.CompilationDatabase(compilation_database_folder)
 else:
-  database = None
+    database = None
 
-SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
+SOURCE_EXTENSIONS = ['.cpp', '.cxx', '.cc', '.c', '.m', '.mm']
+
 
 def DirectoryOfThisScript():
-  return os.path.dirname( os.path.abspath( __file__ ) )
+    return os.path.dirname(os.path.abspath(__file__))
 
 
-def IsHeaderFile( filename ):
-  extension = os.path.splitext( filename )[ 1 ]
-  return extension in [ '.h', '.hxx', '.hpp', '.hh' ]
+def IsHeaderFile(filename):
+    extension = os.path.splitext(filename)[1]
+    return extension in ['.h', '.hxx', '.hpp', '.hh']
 
 
-def GetCompilationInfoForFile( filename ):
-  # The compilation_commands.json file generated by CMake does not have entries
-  # for header files. So we do our best by asking the db for flags for a
-  # corresponding source file, if any. If one exists, the flags for that file
-  # should be good enough.
-  if IsHeaderFile( filename ):
-    basename = os.path.splitext( filename )[ 0 ]
-    for extension in SOURCE_EXTENSIONS:
-      replacement_file = basename + extension
-      if os.path.exists( replacement_file ):
-        compilation_info = database.GetCompilationInfoForFile(
-          replacement_file )
-        if compilation_info.compiler_flags_:
-          return compilation_info
-    return None
-  return database.GetCompilationInfoForFile( filename )
+def GetCompilationInfoForFile(filename):
+    # The compilation_commands.json file generated by CMake does not have entries
+    # for header files. So we do our best by asking the db for flags for a
+    # corresponding source file, if any. If one exists, the flags for that file
+    # should be good enough.
+    if IsHeaderFile(filename):
+        basename = os.path.splitext(filename)[0]
+        for extension in SOURCE_EXTENSIONS:
+            replacement_file = basename + extension
+            if os.path.exists(replacement_file):
+                compilation_info = database.GetCompilationInfoForFile(
+                    replacement_file)
+                if compilation_info.compiler_flags_:
+                    return compilation_info
+        return None
+    return database.GetCompilationInfoForFile(filename)
 
 
-def FlagsForFile( filename, **kwargs ):
-  if not database:
-    return {
-      'flags': flags,
-      'include_paths_relative_to_dir': DirectoryOfThisScript()
-    }
+def FlagsForFile(filename, **kwargs):
+    if not database:
+        return {
+            'flags': flags,
+            'include_paths_relative_to_dir': DirectoryOfThisScript()
+        }
 
-  compilation_info = GetCompilationInfoForFile( filename )
-  if not compilation_info:
-    return None
+    compilation_info = GetCompilationInfoForFile(filename)
+    if not compilation_info:
+        return None
 
-  # Bear in mind that compilation_info.compiler_flags_ does NOT return a
-  # python list, but a "list-like" StringVec object.
-  final_flags = list( compilation_info.compiler_flags_ )
+    # Bear in mind that compilation_info.compiler_flags_ does NOT return a
+    # python list, but a "list-like" StringVec object.
+    final_flags = list(compilation_info.compiler_flags_)
 
-  # NOTE: This is just for YouCompleteMe; it's highly likely that your project
-  # does NOT need to remove the stdlib flag. DO NOT USE THIS IN YOUR
-  # ycm_extra_conf IF YOU'RE NOT 100% SURE YOU NEED IT.
-  # try:
+    # NOTE: This is just for YouCompleteMe; it's highly likely that your project
+    # does NOT need to remove the stdlib flag. DO NOT USE THIS IN YOUR
+    # ycm_extra_conf IF YOU'RE NOT 100% SURE YOU NEED IT.
+    # try:
     # final_flags.remove( '-stdlib=libc++' )
-  # except ValueError:
+    # except ValueError:
     # pass
 
-  return {
-    'flags': final_flags,
-    'include_paths_relative_to_dir': compilation_info.compiler_working_dir_
-  }
+    return {
+        'flags': final_flags,
+        'include_paths_relative_to_dir': compilation_info.compiler_working_dir_
+    }
