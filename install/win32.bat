807192782a1b15d1cf6d10baa014ef6bae9d5489
damu <damu@pinduoduo.com>
2018-08-30 21:35:54 +0800

    remove go-completer in YouCompleteMe

--- install/win32.bat
+++ install/win32.bat
@@ -2,12 +2,15 @@
 ECHO [lin-vim] Install for Windows
 
 mkdir %HOMEPATH%\.ssh
+
 curl -fLo %HOMEPATH%\.vim\autoload\plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo %HOMEPATH%\vimfiles\autoload\plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp %HOMEPATH%\.vim\settings\basic.vimrc %HOMEPATH%\_vimrc
+gvim -c "PlugInstall" -c "qall"
 cp %HOMEPATH%\.vim\lin-vim.vimrc %HOMEPATH%\_vimrc
 gvim -c "PlugInstall" -c "qall"
 cd %HOMEPATH%\.vim\plugged\YouCompleteMe
-python install.py --clang-completer --go-completer --java-completer
+python install.py --clang-completer --java-completer
 rm -rf %HOMEPATH%\vimfiles
 
 pip install pyOpenSSL pyflakes pep8 flake8 pylint cpplint pyOpenSSL autopep8 pathlib
