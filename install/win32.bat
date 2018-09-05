84bb7e1cf3f0a6f6dba99d77e1876becd2aa5e10
damu <damu@pinduoduo.com>
2018-09-05 12:46:51 +0800

    fix npm exit

--- install/win32.bat
+++ install/win32.bat
@@ -1,18 +1,19 @@
 echo "[lin-vim] Install for Windows"
 
-git clone https://github.com/linrongbin16/lin-vim %HOMEPATH%\.vim
 mkdir %HOMEPATH%\.ssh
-cd %HOMEPATH%\.vim
 
+REM install vim
+git clone https://github.com/linrongbin16/lin-vim %HOMEPATH%\.vim
+cd %HOMEPATH%\.vim
 curl -fLo %HOMEPATH%\.vim\autoload\plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+REM install YouCompleteMe
 cp %HOMEPATH%\.vim\lin-vim.vimrc %HOMEPATH%\_vimrc
 vim -c "PlugInstall" -c "qall"
 cd %HOMEPATH%\.vim\plugged\YouCompleteMe
-python install.py --clang-completer --go-completer --java-completer --js-completer
-cd %HOMEPATH%\.vim
+REM python install.py --clang-completer --go-completer --java-completer --js-completer
 
-npm config set unsafe-perm true
-npm install -g js-beautify standard eslint xo typescript-formatter sass remark-cli
-
-python -m pip install pyOpenSSL pyflakes pep8 flake8 pylint cpplint pyOpenSSL autopep8 pathlib
+REM install node/python plugin
+call npm config set unsafe-perm true
+call npm install js-beautify standard eslint xo typescript-formatter sass remark-cli -g
+call python -m pip install pyOpenSSL pyflakes pep8 flake8 pylint cpplint pyOpenSSL autopep8 pathlib
