26468994342342f0c1630ef4dae73343400ecebf
damu <damu@pinduoduo.com>
2018-09-13 15:53:19 +0800

    remove -p for mkdir in win32

--- install/win32.bat
+++ install/win32.bat
@@ -20,21 +20,21 @@ call npm install js-beautify standard eslint xo typescript-formatter sass remark
 call python -m pip install --user pyOpenSSL pyflakes pep8 flake8 pylint cpplint autopep8 pathlib autopep8 yapf urllib3 shutil
 
 rem install golang plugin
-mkdir -p %USERPROFILE%\go
-mkdir -p %USERPROFILE%\go\src
-mkdir -p %USERPROFILE%\go\bin
-mkdir -p %USERPROFILE%\go\pkg
-mkdir -p %USERPROFILE%\workspace
-mkdir -p %USERPROFILE%\workspace\practice
-mkdir -p %USERPROFILE%\workspace\project
+mkdir %USERPROFILE%\go
+mkdir %USERPROFILE%\go\src
+mkdir %USERPROFILE%\go\bin
+mkdir %USERPROFILE%\go\pkg
+mkdir %USERPROFILE%\workspace
+mkdir %USERPROFILE%\workspace\practice
+mkdir %USERPROFILE%\workspace\project
 
 rem install wget.exe
-mkdir -p %USERPROFILE%\.vim\bin
+mkdir %USERPROFILE%\.vim\bin
 cd %USERPROFILE%\.vim\bin
 call python %USERPROFILE%\.vim\install\download.py https://eternallybored.org/misc/wget/1.19.4/64/wget.exe wget.exe
 
 rem install ag.exe
-mkdir -p temp
+mkdir temp
 cd temp
 call python %USERPROFILE%\.vim\install\download.py https://github.com/k-takata/the_silver_searcher-win32/releases/download/2018-08-08%2F2.2.0-2-gbd82cd3/ag-2018-08-08_2.2.0-2-gbd82cd3-x64.zip ag.zip
 call python %USERPROFILE%\.vim\install\extract.py ag.zip
@@ -43,7 +43,7 @@ mv temp\ag.exe ..
 
 rem install ctags.exe readtags.exe
 rm -rf temp
-mkdir -p temp
+mkdir temp
 cd temp
 call python %USERPROFILE%\.vim\install\download.py https://github.com/universal-ctags/ctags-win32/releases/download/2018-09-12%2F41311ac7/ctags-2018-09-12_41311ac7-x64.zip ctags.zip
 call python %USERPROFILE%\.vim\install\extract.py ctags.zip
