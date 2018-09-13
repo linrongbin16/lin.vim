9b554f4c40b643d3d05aea77d8cf586e292c6bdd
damu <damu@pinduoduo.com>
2018-09-13 12:08:08 +0800

    update golang, workspace path

--- install/win32.bat
+++ install/win32.bat
@@ -1,19 +1,29 @@
+@echo off
 echo "[lin-vim] Install for Windows"
 
 mkdir %USERPROFILE%\.ssh
 
-REM install vim
+rem install vim
 git clone https://github.com/linrongbin16/lin-vim %USERPROFILE%\.vim
 cd %USERPROFILE%\.vim
 curl -fLo %USERPROFILE%\.vim\autoload\plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-REM install YouCompleteMe
+rem install YouCompleteMe
 cp %USERPROFILE%\.vim\lin-vim.vimrc %USERPROFILE%\_vimrc
 vim -c "PlugInstall" -c "qall"
 cd %USERPROFILE%\.vim\plugged\YouCompleteMe
 python install.py --clang-completer --go-completer --java-completer --js-completer
 
-REM install node/python plugin
+rem install node/python plugin
 call npm config set unsafe-perm true
 call npm install js-beautify standard eslint xo typescript-formatter sass remark-cli -g
 call python -m pip install --user pyOpenSSL pyflakes pep8 flake8 pylint cpplint autopep8 pathlib autopep8 yapf
+
+rem install golang plugin
+mkdir -p %USERPROFILE%\go
+mkdir -p %USERPROFILE%\go\src
+mkdir -p %USERPROFILE%\go\bin
+mkdir -p %USERPROFILE%\go\pkg
+mkdir -p %USERPROFILE%\workspace
+mkdir -p %USERPROFILE%\workspace\practice
+mkdir -p %USERPROFILE%\workspace\project
