fc499c224863487242a1ecd2e10f77d3da892532
linrongbin <linrongbin16@gmail.com>
2019-02-01 10:21:37 +0800

    remove useless scripts

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -22,7 +22,6 @@ sudo apt-get install -y libssl-dev openssh-server libcrypto++-dev
 sudo apt-get install -y silversearcher-ag unzip bzip2 unrar shadowsocks bc
 sudo apt-get install -y python3 python3-dev python3-pip
 sudo pip3 install pyOpenSSL pyflakes pep8 flake8 pylint cpplint autopep8 pathlib autopep8 yapf
-sudo npm install -g js-beautify standard eslint xo typescript-formatter sass remark-cli tern typescript
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
