da536e52a110c7a18b3351900df6b245e0fa9fa9
linrongbin <linrongbin16@gmail.com>
2019-02-05 00:54:05 +0800

    update win32 install script

--- install/win32.bat
+++ install/win32.bat
@@ -16,17 +16,10 @@ python install.py --clang-completer
 rem install python plugin
 call pip install --user pyOpenSSL pyflakes pep8 flake8 pylint cpplint autopep8 pathlib autopep8 yapf urllib3
 
-rem install plugin
-mkdir %USERPROFILE%\workspace
-mkdir %USERPROFILE%\workspace\practice
-mkdir %USERPROFILE%\workspace\project
-
 cd %USERPROFILE%\.vim
 
 echo.
 echo.
-echo [lin-vim] NOTICE
-echo [lin-vim] install `https://github.com/k-takata/the_silver_searcher-win32/releases` x86_64 zip, extract zip and put `ag.exe` to `%%USERPROFILE%%\.vim\bin\`
-echo [lin-vim] install `https://github.com/universal-ctags/ctags-win32/releases` x86_64 zip, extract zip and put `ctags.exe`, `readtags.exe` to `%%USERPROFILE%%\.vim\bin\`
-echo [lin-vim] install fonts in %%USERPROFILE%%\.vim\guifonts
-echo [lin-vim] add `%%USERPROFILE%%\.vim\bin` to `%%PATH%%`
+echo [lin-vim] install ag.exe from `https://github.com/k-takata/the_silver_searcher-win32/releases`, make sure it's available in %%PATH%%
+echo [lin-vim] install ctags.exe, readtags.exe from `https://github.com/universal-ctags/ctags-win32/releases`, make sure they're available in %%PATH%%
+echo [lin-vim] install fonts in %%USERPROFILE%%\.vim\guifonts\
