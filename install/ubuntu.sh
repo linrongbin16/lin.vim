85a4b4fd0b1f79ed690cc9aa9217f8cb1ad12546
linrongbin <linrongbin16@gmail.com>
2019-02-09 05:00:10 +0000

    fix ubuntu nodejs

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -20,7 +20,7 @@ sudo apt-get install -y git vim vim-gtk libcanberra-gtk-module curl wget zsh
 sudo apt-get install -y gcc g++ clang autoconf automake cmake clang-format pkg-config build-essential
 sudo apt-get install -y libssl-dev openssh-server libcrypto++-dev
 sudo apt-get install -y silversearcher-ag unzip bzip2 unrar shadowsocks bc
-sudo apt-get install -y python3 python3-dev python3-pip node
+sudo apt-get install -y python3 python3-dev python3-pip nodejs
 sudo pip3 install pyOpenSSL pep8 flake8 pylint autopep8 yapf cpplint
 sudo npm install -g --unsafe-perm js-beautify eslint tslint typescript-formatter remark-cli
 sudo apt-get update -y
