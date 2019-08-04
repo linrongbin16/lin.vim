bae87caea794b896c3963388235cd1d432ea9eb5
linrongbin <rolin@ebay.com>
2019-08-04 09:46:20 +0800

    add sqlformat

--- install/manjaro.sh
+++ install/manjaro.sh
@@ -17,7 +17,7 @@ yes | sudo pacman -S gcc clang make autoconf automake cmake pkg-config
 yes | sudo pacman -S openssh openssl crypto++
 yes | sudo pacman -S the_silver_searcher unrar unzip bzip2 shadowsocks-qt5 bc
 yes | sudo pacman -S python python-pip universal-ctags node npm
-sudo pip3 install pyOpenSSL pep8 flake8 pylint autopep8 yapf cpplint
+sudo pip3 install pyOpenSSL pep8 flake8 pylint autopep8 yapf cpplint sqlparse sqlformat
 sudo npm install -g --unsafe-perm js-beautify eslint tslint typescript-formatter
 if [ -d ~/.config ]; then
     sudo chmod -R +rwx ~/.config
