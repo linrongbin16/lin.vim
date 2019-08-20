39044ad9484e15505396a0a4542b585e207a4aa6
linrongbin <rolin@ebay.com>
2019-08-20 09:36:51 +0800

    update ycm header

--- settings/ycm_global_conf.py
+++ settings/ycm_global_conf.py
@@ -79,28 +79,30 @@ flags = [
 ]
 
 # Auto Header Extension Begin
+
 import os
 import subprocess
 
 
-def IsAscii(s):
+def is_ascii_char(s):
     return all(ord(c) < 128 for c in s)
 
 
-def RunProc(*cmd):
+def run_process(*params):
     try:
-        proc = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc = subprocess.Popen(params,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
         stdout_str = iter(proc.stdout.readline, b"")
         stderr_str = iter(proc.stderr.readline, b"")
     except subprocess.CalledProcessError:
-        exit(1)
+        return None, None
     outstr = [x.decode() for x in stdout_str if len(x) > 0]
     errstr = [x.decode() for x in stderr_str if len(x) > 0]
     return outstr, errstr
 
 
-def ListDir(base_dir, target, depth):
+def list_directory(base_dir, target, depth):
     target_dir = os.path.join(os.getcwd(), base_dir)
     num_sep = target_dir.count(os.path.sep)
     dir_list = []
@@ -117,160 +119,151 @@ def ListDir(base_dir, target, depth):
     return [d.replace('/', '\\') for d in dir_list]
 
 
-def GitRoot():
-    root, _ = RunProc('git', 'rev-parse', '--show-toplevel')
-    return root[0].strip() if (len(root) > 0) else None
-
-
-def GitHeader():
-    groot = GitRoot()
-    if groot is None:
-        return []
-    return ListDir(groot, 'include', 20)
-
-
-def IncludeBrew():
-    brew_home = '/usr/local/Cellar'
-    return ListDir(brew_home, 'include', 2)
-
-
-def MacOSHeader():
+def git_header():
     try:
-        header = []
-        header.append('-I')
-        header.append(
-            '/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include')
-        header.append('-I')
-        header.append(
-            '/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1'
-        )
-        for inc in IncludeBrew():
-            header.append('-I')
-            header.append(inc)
-        for inc in GitHeader():
-            header.append('-I')
-            header.append(inc)
-        return header
-    except Exception:
+        root, _ = run_process('git', 'rev-parse', '--show-toplevel')
+        groot = root[0].strip() if (len(root) > 0) else None
+        return list_directory(groot, 'include', 20)
+    except:
         return []
 
 
-def LinuxHeader():
+def homebrew_header():
     try:
-        header = []
-        header.append('-I')
-        header.append('/usr/include')
-        header.append('-I')
-        header.append('/usr/include/c++')
-        header.append('-I')
-        header.append('/usr/lib')
-        header.append('-I')
-        header.append('/usr/include/x86_64-linux-gnu')
-        # '-I',
-        # '/usr/include/c++/7',
-        for version in os.listdir('/usr/include/c++'):
-            header.append('-I')
-            header.append('/usr/include/c++/%s' % (version))
-        for inc in GitHeader():
-            header.append('-I')
-            header.append(inc)
-        return header
-    except Exception:
+        header = '/usr/local/Cellar'
+        return list_directory(header, 'include', 2)
+    except:
         return []
 
 
-def WindowsHeader():
+def os_listdir_wrapper(d):
     try:
-        header = []
-        # '-I',
-        # 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Tools\\MSVC\\14.14.26428\\include',
-        for release in os.listdir(
-                'C:\\Program Files (x86)\\Microsoft Visual Studio\\'):
-            try:
-                for version in os.listdir(
-                        'C:\\Program Files (x86)\\Microsoft Visual Studio\\%s\\Community\\VC\\Tools\\MSVC\\'
-                        % (release)):
-                    header.append('-I')
-                    header.append(
-                        'C:\\Program Files (x86)\\Microsoft Visual Studio\\%s\\Community\\VC\\Tools\\MSVC\\%s\\include'
-                        % (release, version))
-            except Exception:
-                pass
-        # '-I',
-        # 'C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.17134.0\\ucrt',
-        for version in os.listdir(
-                'C:\\Program Files (x86)\\Windows Kits\\10\\Include\\'):
+        return os.listdir(d)
+    except:
+        return []
+
+
+def macos_header():
+    header = []
+    header.append('-I')
+    header.append(
+        '/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include')
+    header.append('-I')
+    header.append(
+        '/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1'
+    )
+    for inc in homebrew_header():
+        header.append('-I')
+        header.append(inc)
+    for inc in git_header():
+        header.append('-I')
+        header.append(inc)
+    return header
+
+
+def linux_header():
+    header = []
+    header.append('-I')
+    header.append('/usr/include')
+    header.append('-I')
+    header.append('/usr/include/c++')
+    header.append('-I')
+    header.append('/usr/lib')
+    header.append('-I')
+    header.append('/usr/include/x86_64-linux-gnu')
+    # '-I',
+    # '/usr/include/c++/7',
+    for version in os_listdir_wrapper('/usr/include/c++'):
+        header.append('-I')
+        header.append('/usr/include/c++/%s' % (version))
+    for inc in git_header():
+        header.append('-I')
+        header.append(inc)
+    return header
+
+
+def windows_header():
+    header = []
+    # '-I',
+    # 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Tools\\MSVC\\14.14.26428\\include',
+    for release in os_listdir_wrapper(
+            'C:\\Program Files (x86)\\Microsoft Visual Studio\\'):
+        for version in os_listdir_wrapper(
+                'C:\\Program Files (x86)\\Microsoft Visual Studio\\%s\\Community\\VC\\Tools\\MSVC\\'
+                % (release)):
             header.append('-I')
             header.append(
-                'C:\\Program Files (x86)\\Windows Kits\\10\\Include\\%s\\ucrt'
-                % (version))
-        for inc in GitHeader():
-            header.append('-I')
-            header.append(inc)
-        return header
-    except Exception:
-        return []
+                'C:\\Program Files (x86)\\Microsoft Visual Studio\\%s\\Community\\VC\\Tools\\MSVC\\%s\\include'
+                % (release, version))
+    # '-I',
+    # 'C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.17134.0\\ucrt',
+    for version in os_listdir_wrapper(
+            'C:\\Program Files (x86)\\Windows Kits\\10\\Include\\'):
+        header.append('-I')
+        header.append(
+            'C:\\Program Files (x86)\\Windows Kits\\10\\Include\\%s\\ucrt' %
+            (version))
+    for inc in git_header():
+        header.append('-I')
+        header.append(inc)
+    return []
 
 
-def UserHeader():
-    try:
-        header = []
-        rootpath = [
-            '/', 'C:\\', 'D:\\', 'E:\\', 'F:\\', 'G:\\', 'H:\\', 'I:\\',
-            'G:\\', 'K:\\', 'L:\\', 'M:\\', 'N:\\', 'O:\\', 'P:\\', 'Q:\\',
-            'R:\\', 'S:\\', 'T:\\', 'U:\\', 'V:\\', 'W:\\', 'X:\\', 'Y:\\',
-            'Z:\\', 'A:\\', 'B:\\'
-        ]
-        cur = '.'
+def user_header():
+    header = []
+    rootpath = [
+        '/', 'C:\\', 'D:\\', 'E:\\', 'F:\\', 'G:\\', 'H:\\', 'I:\\', 'G:\\',
+        'K:\\', 'L:\\', 'M:\\', 'N:\\', 'O:\\', 'P:\\', 'Q:\\', 'R:\\', 'S:\\',
+        'T:\\', 'U:\\', 'V:\\', 'W:\\', 'X:\\', 'Y:\\', 'Z:\\', 'A:\\', 'B:\\'
+    ]
+    cur = '.'
+    header.append('-I')
+    header.append(cur)
+    for i in range(10):
+        if os.path.abspath(cur) in rootpath:
+            break
+        cur = '%s%s' % (cur, '/..')
+        if ' ' in cur:
+            continue
+        if not is_ascii_char(cur):
+            continue
         header.append('-I')
         header.append(cur)
-        for i in range(10):
-            if os.path.abspath(cur) in rootpath:
-                break
-            cur = '%s%s' % (cur, '/..')
-            if ' ' in cur:
+        for path in os_listdir_wrapper(cur):
+            target = '%s/%s' % (cur, path)
+            if ' ' in target:
                 continue
-            if not IsAscii(cur):
-                continue
-            header.append('-I')
-            header.append(cur)
-            for path in os.listdir(cur):
-                target = '%s/%s' % (cur, path)
-                if ' ' in target:
-                    continue
-                if not IsAscii(target):
-                    continue
-                if path.startswith('.'):
-                    continue
-                if not os.path.isdir(target):
-                    continue
-                header.append('-I')
-                header.append(target)
-        for path in os.listdir('.'):
-            if ' ' in path:
-                continue
-            if not IsAscii(path):
+            if not is_ascii_char(target):
                 continue
             if path.startswith('.'):
                 continue
-            if not os.path.isdir(path):
+            if not os.path.isdir(target):
                 continue
             header.append('-I')
-            header.append(path)
-        return header
-    except Exception:
-        return []
+            header.append(target)
+    for path in os_listdir_wrapper('.'):
+        if ' ' in path:
+            continue
+        if not is_ascii_char(path):
+            continue
+        if path.startswith('.'):
+            continue
+        if not os.path.isdir(path):
+            continue
+        header.append('-I')
+        header.append(path)
+    return header
 
 
 # Auto Header Extension End
 
 if platform.system() == 'Windows':
-    flags.extend(WindowsHeader())
+    flags.extend(windows_header())
 elif platform.system() == 'Darwin':
-    flags.extend(MacOSHeader())
+    flags.extend(macos_header())
 else:
-    flags.extend(LinuxHeader())
-flags.extend(UserHeader())
+    flags.extend(linux_header())
+flags.extend(user_header())
 
 # Clang automatically sets the '-std=' flag to 'c++14' for MSVC 2015 or later,
 # which is required for compiling the standard library, and to 'c++11' for older
--- settings/youcompleteme.vim
+++ settings/youcompleteme.vim
@@ -19,7 +19,7 @@ let g:ycm_complete_in_comments=1
 let g:ycm_collect_identifiers_from_comments_and_strings=1
 let g:ycm_collect_identifiers_from_tags_files=1
 let g:ycm_seed_identifiers_with_syntax=1
-let g:ycm_cache_omnifunc=0
+let g:ycm_cache_omnifunc=1
 let g:ycm_confirm_extra_conf=0
 
 let g:ycm_key_invoke_completion = '<C-k>'
