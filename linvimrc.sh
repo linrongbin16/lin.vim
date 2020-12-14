ee13acc42152f2b85a26c885696fee9850c18d69
linrongbin <linrongbin16@outlook.com>
2020-12-14 14:29:31 +0800

    remove CURL

--- linvimrc.sh
+++ linvimrc.sh
@@ -13,9 +13,6 @@ export LANGUAGE='en_US.UTF-8'
 # lin.vim vcmd
 export PATH=~/.vim/command:$PATH
 
-# macOS homebrew curl
-export HOMEBREW_FORCE_BREWED_CURL=1
-
 # prompt color scheme
 if [[ $TERM = *256color* || $TERM = *rxvt* ]]; then
     _prompt_color=(
