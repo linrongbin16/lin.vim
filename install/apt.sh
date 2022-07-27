6b9cbe1d576dfc8e2af0b32e8800ddcbae697690
linrongbin16 <linrongbin16@outlook.com>
2022-07-28 06:37:09 +0800

    add system clipboard for copy paste

--- install/apt.sh
+++ install/apt.sh
@@ -20,6 +20,7 @@ $INSTALL/install_or_skip.sh "sudo apt-get install -y bzip2" "bzip2"
 $INSTALL/install_or_skip.sh "sudo apt-get install -y unzip" "unzip"
 $INSTALL/install_or_skip.sh "sudo apt-get install -y zip" "zip"
 $INSTALL/install_or_skip.sh "sudo apt-get install -y p7zip-full" "7z"
+sudo apt-get install -y xclip wl-clipboard
 
 # python3
 $INSTALL/install_or_skip.sh "sudo apt-get install -y python3 python3-dev python3-venv python3-pip python3-docutils" "python3"
