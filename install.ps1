1072e668fd0080df0a535f39e20740781c4f2080
linrongbin16 <linrongbin16@outlook.com>
2022-10-21 13:04:14 +0800

    save

--- install.ps1
+++ install.ps1
@@ -5,85 +5,106 @@ $APPDATA_LOCAL_HOME="$env:UserProfile\AppData\Local"
 $NVIM_HOME="$APPDATA_LOCAL_HOME\nvim"
 $TEMPLATE_HOME="$VIM_HOME\template"
 
-Function Message($msg) {
-    Write-Host "[lin.vim] - $msg"
+Function Message() {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true)][String]$message
+    )
+    Write-Host "[lin.vim] - $message"
 }
 
-Function InstallOrSkip($command, $target) {
+Function InstallOrSkip() {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true)][String]$command,
+        [Parameter(Mandatory = $true)][String]$target
+    )
     if (Get-Command -Name $target -ErrorAction SilentlyContinue) {
-        Message -msg "'${target}' already exist, skip..."
+        Message "'${target}' already exist, skip..."
     } else {
-        Message -msg "install '${target}' with command: '${command}'"
+        Message "install '${target}' with command: '${command}'"
         Invoke-Expression $command
     }
 }
 
-Function TryBackup($src) {
-    If (Test-Path $src) {
+Function TryBackup() {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true)][String]$target
+    )
+    If (Test-Path $target) {
         $now=Get-Date -Format "yyyy-MM-dd.HH-mm-ss"
-        $target=-join($src, ".", $now)
-        Message -msg "backup '$src' to '$target'"
-        Rename-Item $src $target
+        $backup=-join($target, ".", $now)
+        Message "backup '$target' to '$backup'"
+        Rename-Item $target $backup
     }
 }
 
-Function TryDelete($src) {
-    If (Test-Path $src) {
-        Message -msg "delete '$src'"
-        (Get-Item $src).Delete()
+Function TryDelete() {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true)][String]$target
+    )
+    If (Test-Path $target) {
+        Message "delete '$target'"
+        (Get-Item $target).Delete()
     }
 }
 
 Function RustDependency() {
-    Message -msg "install modern rust commands with cargo"
+    Message "install modern rust commands with cargo"
     InstallOrSkip -command "cargo install ripgrep" -target "rg"
     InstallOrSkip -command "cargo install fd-find" -target "fd"
     InstallOrSkip -command "cargo install --locked bat" -target "bat"
 }
 
 Function Pip3Dependency() {
-    Message -msg "install python packages with pip3"
+    Message "install python packages with pip3"
     pip3 install pyOpenSSL neovim pynvim pep8 flake8 pylint black yapf chardet cmakelang cmake-language-server click
 }
 
 Function NpmDependency() {
-    Message -msg "install node packages with npm"
+    Message "install node packages with npm"
     npm install -g yarn prettier neovim
 }
 
 Function InstallTemplates() {
-    Message -msg "install configurations from templates"
+    Message "install configurations from templates"
     Copy-Item $TEMPLATE_HOME\plugin-template.vim -Destination $VIM_HOME\plugin.vim
     Copy-Item $TEMPLATE_HOME\coc-settings-template.json -Destination $VIM_HOME\coc-settings.json
     Copy-Item $TEMPLATE_HOME\setting-template.vim -Destination $VIM_HOME\setting.vim
 }
 
 Function InstallVimrc() {
-    Message -msg "install .vimrc for vim"
-    TryBackup -src "$env:UserProfile\_vimrc"
+    Message "install .vimrc for vim"
+    TryBackup "$env:UserProfile\_vimrc"
     cmd /c mklink %USERPROFILE%\_vimrc %USERPROFILE%\.vim\lin.vim
 }
 
 Function InstallNvimInit() {
-    Message -msg "install $APPDATA_LOCAL_HOME/nvim and $APPDATA_LOCAL_HOME/nvim/init.vim for neovim"
-    TryBackup -src "$NVIM_HOME"
+    Message "install $APPDATA_LOCAL_HOME/nvim and $APPDATA_LOCAL_HOME/nvim/init.vim for neovim"
+    TryBackup "$NVIM_HOME"
     cmd /c mklink %USERPROFILE%\AppData\Local\nvim %USERPROFILE%\.vim
-    TryDelete -src "$NVIM_HOME\init.vim"
+    TryDelete "$NVIM_HOME\init.vim"
     cmd /c mklink %USERPROFILE%\AppData\Local\nvim\init.vim %USERPROFILE%\.vim\lin.vim
 }
 
 Function InstallVimPlugin() {
-    Message -msg "install vim plugins"
+    Message "install vim plugins"
     gvim -E -c "PlugInstall" -c "qall"
 }
 
-Function InstallNvimPluginIfExist() {
-    Message -msg "install neovim plugins"
+Function InstallNvimPlugin() {
+    Message "install neovim plugins"
     nvim -E -c "PlugInstall" -c "qall"
 }
 
 Function Main() {
-    Message -msg "install dependencies for windows"
+    Message "install dependencies for windows"
 
     # install dependencies
     RustDependency
@@ -96,7 +117,7 @@ Function Main() {
     InstallNvimInit
 
     # install plugins
-    InstallNvimPluginIfExist
+    InstallNvimPlugin
     InstallVimPlugin
 }
 
