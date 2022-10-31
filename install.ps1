033ab072ea63ec1957737673c79d26fb78f423e5
linrongbin16 <linrongbin16@outlook.com>
2022-10-31 12:10:01 +0800

    add options for ps1

--- install.ps1
+++ install.ps1
@@ -1,13 +1,27 @@
 $VIM_HOME="$env:UserProfile\.vim"
+$INSTALL_HOME="$VIM_HOME\install"
 $APPDATA_LOCAL_HOME="$env:UserProfile\AppData\Local"
 $NVIM_HOME="$APPDATA_LOCAL_HOME\nvim"
 $TEMPLATE_HOME="$VIM_HOME\template"
+
+$OPT_BASIC=$False
+$OPT_FULL=$True # default mode
+$OPT_WITHOUT_CXX=$False
+$OPT_WITHOUT_PYTHON=$False
+$OPT_WITHOUT_MARKDOWN=$False
+$OPT_WITHOUT_JSON=$False
+$OPT_WITHOUT_JAVASCRIPT=$False
+$OPT_WITHOUT_POWERSHELL=$False
+$OPT_WITHOUT_BASH=$False
+$OPT_WITHOUT_HIGHLIGHT=$False
+$OPT_WITHOUT_COLOR=$False
+$OPT_WITHOUT_GIT=$False
 
 function Message() {
     [CmdletBinding()]
     Param
     (
-        [Parameter(Mandatory = $true)][String]$message
+        [Parameter(Mandatory = $True)][String]$message
     )
     Write-Host "[lin.vim] - $message"
 }
@@ -16,8 +30,8 @@ function InstallOrSkip() {
     [CmdletBinding()]
     Param
     (
-        [Parameter(Mandatory = $true)][String]$command,
-        [Parameter(Mandatory = $true)][String]$target
+        [Parameter(Mandatory = $True)][String]$command,
+        [Parameter(Mandatory = $True)][String]$target
     )
     if (Get-Command -Name $target -ErrorAction SilentlyContinue) {
         Message "'${target}' already exist, skip..."
@@ -31,7 +45,7 @@ function TryBackup() {
     [CmdletBinding()]
     Param
     (
-        [Parameter(Mandatory = $true)][String]$target
+        [Parameter(Mandatory = $True)][String]$target
     )
     if (Test-Path $target) {
         $now=Get-Date -Format "yyyy-MM-dd.HH-mm-ss.fffffff"
@@ -50,7 +64,7 @@ function TryDelete() {
     [CmdletBinding()]
     Param
     (
-        [Parameter(Mandatory = $true)][String]$target
+        [Parameter(Mandatory = $True)][String]$target
     )
     if (Test-Path $target) {
         Message "delete '$target'"
@@ -106,6 +120,75 @@ function InstallNvimPlugin() {
     nvim -E -c "PlugInstall" -c "qall"
 }
 
+function ShowHelp() {
+    Get-Content $INSTALL_HOME/help_message.txt | Write-Host
+}
+
+function CheckNoBasicOption() {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $True)][Boolean]$hasBasicOption,
+        [Parameter(Mandatory = $True)][String]$inputOption
+    )
+    if ($hasBasicOption) {
+        Message "error! cannot use $inputOption along with -basic $True"
+        Exit
+    }
+}
+
+function CheckNoLimitOption() {
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $True)][Boolean]$hasLimitOption,
+        [Parameter(Mandatory = $True)][String]$inputOption
+    )
+    if ($hasLimitOption) {
+        Message "error! cannot use $inputOption along with -limit $True"
+        Exit
+    }
+}
+
+function ParseOptions() {
+    $help=$False
+    $hasBasicOption=$False
+    $hasLimitOption=$False
+    for ($i = 0; $i -lt $args.Count; $i++) {
+        $opt = $args[ $i ]
+        if ($opt -eq "-h" || $opt -eq "--help") {
+            $help=$True
+                break
+        } elseif ($opt -eq "-b" || $opt -eq "--basic") {
+            $hasBasicOption=$true
+            CheckNoLimitOption $hasLimitOption $opt
+            $OPT_BASIC=$True
+            $OPT_FULL=$False
+        } elseif ($opt -eq "-l" || $opt -eq "--limit") {
+            $hasLimitOption=$true
+            CheckNoBasicOption $hasBasicOption $opt
+            $OPT_BASIC=$False
+            $OPT_FULL=$True
+            $OPT_WITHOUT_CXX=$True
+            $OPT_WITHOUT_PYTHON=$True
+            $OPT_WITHOUT_MARKDOWN=$True
+            $OPT_WITHOUT_JSON=$True
+            $OPT_WITHOUT_JAVASCRIPT=$True
+            $OPT_WITHOUT_POWERSHELL=$True
+            $OPT_WITHOUT_BASH=$True
+            $OPT_WITHOUT_HIGHLIGHT=$True
+            $OPT_WITHOUT_COLOR=$True
+        } else {
+            Message "unknown options! please try .\install.ps1 --help for more information"
+            Exit
+        }
+    }
+    if ($help) {
+        ShowHelp
+        Exit
+    }
+}
+
 function Main() {
     Message "install dependencies for windows"
 
@@ -135,7 +218,7 @@ if (!($currentPrincipal.IsInRole($adminRole)))
 {
     # Create a new process that run as administrator
     $installer=$script:MyInvocation.MyCommand.Path
-    Start-Process powershell "& '$installer'" -Verb RunAs
+    Start-Process powershell "& '$installer' @args" -Verb RunAs
 
     # Exit from the current process
     Exit;
--- install.sh
+++ install.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-VIM=$HOME/.vim
-CONFIG=$HOME/.config
-NVIM=$CONFIG/nvim
-INSTALL=$VIM/install
-TEMPLATE=$VIM/template
+VIM_HOME=$HOME/.vim
+CONFIG_HOME=$HOME/.config
+NVIM_HOME=$CONFIG_HOME/nvim
+INSTALL_HOME=$VIM_HOME/install
+TEMPLATE_HOME=$VIM_HOME/template
 OS="$(uname -s)"
 
 OPT_BASIC=0
-OPT_FULL=1
-OPT_DISABLE_CXX=0
-OPT_DISABLE_PYTHON=0
-OPT_DISABLE_MARKDOWN=0
-OPT_DISABLE_JSON=0
-OPT_DISABLE_JAVASCRIPT=0
-OPT_DISABLE_POWERSHELL=0
-OPT_DISABLE_BASH=0
-OPT_DISABLE_SPELLCHECK=0
-OPT_DISABLE_HIGHLIGHT=0
-OPT_DISABLE_COLOR=0
+OPT_FULL=1 # default mode
+OPT_WITHOUT_CXX=0
+OPT_WITHOUT_PYTHON=0
+OPT_WITHOUT_MARKDOWN=0
+OPT_WITHOUT_JSON=0
+OPT_WITHOUT_JAVASCRIPT=0
+OPT_WITHOUT_POWERSHELL=0
+OPT_WITHOUT_BASH=0
+OPT_WITHOUT_HIGHLIGHT=0
+OPT_WITHOUT_COLOR=0
+OPT_WITHOUT_GIT=0
 
 function try_backup() {
     local src=$1
     if [ -f "$src" ]; then
-        $INSTALL/msg.sh "backup '$src' to '$target'"
+        $INSTALL_HOME/msg.sh "backup '$src' to '$target'"
         mv $src $src.$(date +"%Y-%m-%d.%H-%M-%S.%6N")
     fi
 }
@@ -31,7 +31,7 @@ function try_backup() {
 function try_delete() {
     local src=$1
     if [ -f "$src" ]; then
-        $INSTALL/msg.sh "remove '$src'"
+        $INSTALL_HOME/msg.sh "remove '$src'"
         rm -rf $src
     fi
 }
@@ -40,66 +40,66 @@ function platform_dependency() {
     case "$OS" in
         Linux)
             if [ -f "/etc/arch-release" ] || [ -f "/etc/artix-release" ]; then
-                $INSTALL/pacman.sh
+                $INSTALL_HOME/pacman.sh
             elif [ -f "/etc/fedora-release" ] || [ -f "/etc/redhat-release" ]; then
-                $INSTALL/dnf.sh
+                $INSTALL_HOME/dnf.sh
             elif [ -f "/etc/gentoo-release" ]; then
-                $INSTALL/emerge.sh
+                $INSTALL_HOME/emerge.sh
             else
                 # assume apt
-                $INSTALL/apt.sh
+                $INSTALL_HOME/apt.sh
             fi
             ;;
         FreeBSD)
-            $INSTALL/pkg.sh
+            $INSTALL_HOME/pkg.sh
             ;;
         NetBSD)
-            $INSTALL/pkgin.sh
+            $INSTALL_HOME/pkgin.sh
             ;;
         OpenBSD)
-            $INSTALL/pkg_add.sh
+            $INSTALL_HOME/pkg_add.sh
             ;;
         Darwin)
-            $INSTALL/brew.sh
+            $INSTALL_HOME/brew.sh
             ;;
         *)
-            $INSTALL/msg.sh "$OS is not supported, exit..."
+            $INSTALL_HOME/msg.sh "$OS is not supported, exit..."
             exit 1
             ;;
     esac
 }
 
 function rust_dependency() {
-    $INSTALL/install_or_skip.sh "curl https://sh.rustup.rs -sSf | sh -s -- -y" "rustc"
+    $INSTALL_HOME/install_or_skip.sh "curl https://sh.rustup.rs -sSf | sh -s -- -y" "rustc"
     source $HOME/.cargo/env
-    $INSTALL/install_or_skip.sh "cargo install ripgrep" "rg"
-    $INSTALL/install_or_skip.sh "cargo install fd-find" "fd"
-    $INSTALL/install_or_skip.sh "cargo install --locked bat" "bat"
+    $INSTALL_HOME/install_or_skip.sh "cargo install ripgrep" "rg"
+    $INSTALL_HOME/install_or_skip.sh "cargo install fd-find" "fd"
+    $INSTALL_HOME/install_or_skip.sh "cargo install --locked bat" "bat"
 }
 
 function golang_dependency() {
     # see https://github.com/canha/golang-tools-install-script
-    $INSTALL/install_or_skip.sh "wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash" "go"
+    $INSTALL_HOME/install_or_skip.sh "wget -q -O - https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash" "go"
     export PATH=$HOME/.go/bin:$PATH
 }
 
 function pip3_dependency() {
-    $INSTALL/msg.sh "install python packages with pip3"
+    $INSTALL_HOME/msg.sh "install python packages with pip3"
     sudo pip3 install pyOpenSSL neovim pynvim pep8 flake8 pylint black yapf chardet cmakelang cmake-language-server click
 }
 
 function npm_dependency() {
-    $INSTALL/msg.sh "install node packages with npm"
+    $INSTALL_HOME/msg.sh "install node packages with npm"
     sudo npm install -g yarn prettier neovim
 }
 
 function guifont_dependency() {
     if [ "$OS" == "Darwin" ]; then
-        $INSTALL/msg.sh "install hack nerd font with brew"
+        $INSTALL_HOME/msg.sh "install hack nerd font with brew"
         brew tap homebrew/cask-fonts
         brew install --cask font-hack-nerd-font
     else
-        $INSTALL/msg.sh "install hack nerd font from github"
+        $INSTALL_HOME/msg.sh "install hack nerd font from github"
         mkdir -p ~/.local/share/fonts && cd ~/.local/share/fonts
         local font_file=Hack.zip
         local font_version=v2.2.2
@@ -109,78 +109,42 @@ function guifont_dependency() {
         fi
         curl -L $font_url -o $font_file
         if [ $? -ne 0 ]; then
-            $INSTALL/msg.sh "failed to download $font_file, skip..."
+            $INSTALL_HOME/msg.sh "failed to download $font_file, skip..."
         else
             unzip -o $font_file
-            $INSTALL/msg.sh "install hack nerd font from github - done"
+            $INSTALL_HOME/msg.sh "install hack nerd font from github - done"
         fi
     fi
 }
 
 function install_templates() {
-    $INSTALL/msg.sh "install configurations from templates"
-    cp $TEMPLATE/plugin-template.vim $VIM/plugin.vim
-    cp $TEMPLATE/coc-settings-template.json $VIM/coc-settings.json
-    cp $TEMPLATE/setting-template.vim $VIM/setting.vim
+    $INSTALL_HOME/msg.sh "install configurations from templates"
+    cp $TEMPLATE_HOME/plugin-template.vim $VIM_HOME/plugin.vim
+    cp $TEMPLATE_HOME/coc-settings-template.json $VIM_HOME/coc-settings.json
+    cp $TEMPLATE_HOME/setting-template.vim $VIM_HOME/setting.vim
 }
 
 function install_vimrc() {
-    $INSTALL/msg.sh "install .vimrc for vim"
+    $INSTALL_HOME/msg.sh "install .vimrc for vim"
     try_backup $HOME/.vimrc
-    ln -s $VIM/lin.vim $HOME/.vimrc
-    $INSTALL/msg.sh "install vim plugins"
+    ln -s $VIM_HOME/lin.vim $HOME/.vimrc
+    $INSTALL_HOME/msg.sh "install vim plugins"
     vim -E -c "PlugInstall" -c "qall"
 }
 
 function install_nvim_init() {
-    $INSTALL/msg.sh "install ~/.config/nvim and ~/.config/nvim/init.vim for neovim"
-    mkdir -p $CONFIG
-    try_backup $NVIM/init.vim
-    try_backup $NVIM
-    ln -s $VIM $NVIM
-    ln -s $VIM/lin.vim $NVIM/init.vim
-    $INSTALL/msg.sh "install neovim plugins"
+    $INSTALL_HOME/msg.sh "install ~/.config/nvim and ~/.config/nvim/init.vim for neovim"
+    mkdir -p $CONFIG_HOME
+    try_backup $NVIM_HOME/init.vim
+    try_backup $NVIM_HOME
+    ln -s $VIM_HOME $NVIM_HOME
+    ln -s $VIM_HOME/lin.vim $NVIM_HOME/init.vim
+    $INSTALL_HOME/msg.sh "install neovim plugins"
     nvim -E -c "PlugInstall" -c "qall"
 }
 
 function show_help() {
-cat << EOF
-Usage: ./install.sh [options]
-
-Installer has 3 modes: basic, limit and full.
-
-Basic mode only install pure vim script settings, no vim plugins or any other third party softwares.
-It is for production environment, which lacks of software sources or user authentication.
-
-Limit mode installs limited features, no extra highlights, colors or all other language supports.
-It is for better performance on some old devices, which lacks of CPU, memory or graphics.
-
-Full mode is default mode, enable all features.
-It is for best user experience, while consumes more CPU, memory and graphics.
-You could use extra disable options to disable some specific feature.
-Notice `--disable-all-language --disable-highlight --disable-color` is equivalent to `--limit`.
-
--h,--help       Show help message.
-
--b,--basic      Install pure vim script settings, no vim plugins or any other third party softwares.
-                This mode is for production environment, which lacks of third party support.
-
--l,--limit      Install limited features, no extra highlight, extra colors, all other language supports.
-                This mode is for old devices, disable some features for better performance.
-
---disable-cxx                   Disable c/c++/cmake support.
---disable-python                Disable python support.
---disable-markdown              Disable markdown support.
---disable-json                  Disable json support.
---disable-javascript            Disable javascript support.
---disable-powershell            Disable powershell support.
---disable-bash                  Disable linux bash support.
---disable-spellcheck            Disable english spell check.
---disable-all-language          Disable all language supports above.
-
---disable-highlight             Disable extra highlights such as cursor word highlight, fzf preview syntax highlight, etc.
---disable-color                 Disable extra colors such as RGBs, random colorschemes, etc.
-EOF
+    cat $INSTALL_HOME/help_message.txt
 }
 
 function check_no_basic_option() {
@@ -189,7 +153,7 @@ function check_no_basic_option() {
     has_basic_option=$1
     input_option=$2
     if [ $has_basic_option -eq 1 ]; then
-        $INSTALL/msg.sh "error! cannot use $input_option along with --basic"
+        $INSTALL_HOME/msg.sh "error! cannot use $input_option along with --basic"
         exit 0
     fi
 }
@@ -200,14 +164,14 @@ function check_no_limit_option() {
     has_limit_option=$1
     input_option=$2
     if [ $has_limit_option -eq 1 ]; then
-        $INSTALL/msg.sh "error! cannot use $input_option along with --limit"
+        $INSTALL_HOME/msg.sh "error! cannot use $input_option along with --limit"
         exit 0
     fi
 }
 
 function parse_options() {
     local options
-    options=$(getopt -l "help,basic,limit,disable-cxx,disable-python,disable-json,disable-javascript,disable-powershell,disable-all-language,disable-highlight,disable-color,disable-spellcheck" -o "hbl" -a -- "$@")
+    options=$(getopt -l "help,basic,limit,without-cxx,without-python,without-json,without-javascript,without-powershell,without-all-language,without-highlight,without-color,without-git" -o "hbl" -a -- "$@")
     eval set -- "$options"
 
     local has_basic_option=0
@@ -235,69 +199,68 @@ function parse_options() {
 
         OPT_BASIC=0
         OPT_FULL=1
-        OPT_DISABLE_CXX=1
-        OPT_DISABLE_PYTHON=1
-        OPT_DISABLE_MARKDOWN=1
-        OPT_DISABLE_JSON=1
-        OPT_DISABLE_JAVASCRIPT=1
-        OPT_DISABLE_POWERSHELL=1
-        OPT_DISABLE_BASH=1
-        OPT_DISABLE_SPELLCHECK=1
-        OPT_DISABLE_HIGHLIGHT=1
-        OPT_DISABLE_COLOR=1
+        OPT_WITHOUT_CXX=1
+        OPT_WITHOUT_PYTHON=1
+        OPT_WITHOUT_MARKDOWN=1
+        OPT_WITHOUT_JSON=1
+        OPT_WITHOUT_JAVASCRIPT=1
+        OPT_WITHOUT_POWERSHELL=1
+        OPT_WITHOUT_BASH=1
+        OPT_WITHOUT_HIGHLIGHT=1
+        OPT_WITHOUT_COLOR=1
         ;;
-    --disable-cxx) 
+    --without-cxx) 
         shift
         check_no_basic_option $has_basic_option "$1"
-        OPT_DISABLE_CXX=1
+        OPT_WITHOUT_CXX=1
         ;;
-    --disable-python) 
+    --without-python) 
         shift
         check_no_basic_option $has_basic_option "$1"
-        OPT_DISABLE_PYTHON=1
+        OPT_WITHOUT_PYTHON=1
         ;;
-    --disable-markdown) 
+    --without-markdown) 
         shift
         check_no_basic_option $has_basic_option "$1"
-        OPT_DISABLE_MARKDOWN=1
+        OPT_WITHOUT_MARKDOWN=1
         ;;
-    --disable-json) 
+    --without-json) 
         shift
         check_no_basic_option $has_basic_option "$1"
-        OPT_DISABLE_JSON=1
+        OPT_WITHOUT_JSON=1
         ;;
-    --disable-javascript) 
+    --without-javascript) 
         shift
         check_no_basic_option $has_basic_option "$1"
-        OPT_DISABLE_JAVASCRIPT=1
+        OPT_WITHOUT_JAVASCRIPT=1
         ;;
-    --disable-powershell) 
+    --without-powershell) 
         shift
         check_no_basic_option $has_basic_option "$1"
-        OPT_DISABLE_POWERSHELL=1
+        OPT_WITHOUT_POWERSHELL=1
         ;;
-    --disable-bash) 
+    --without-bash) 
         shift
         check_no_basic_option $has_basic_option "$1"
-        OPT_DISABLE_BASH=1
+        OPT_WITHOUT_BASH=1
         ;;
-    --disable-spellcheck) 
+    --without-highlight) 
         shift
         check_no_basic_option $has_basic_option "$1"
-        OPT_DISABLE_SPELLCHECK=1
+        OPT_WITHOUT_HIGHLIGHT=1
         ;;
-    --disable-highlight) 
+    --without-color)
         shift
         check_no_basic_option $has_basic_option "$1"
-        OPT_DISABLE_HIGHLIGHT=1
+        OPT_WITHOUT_COLOR=1
         ;;
-    --disable-color)
+    --without-git)
         shift
         check_no_basic_option $has_basic_option "$1"
-        OPT_DISABLE_COLOR=1
+        OPT_WITHOUT_GIT=1
         ;;
     *)
-        $INSTALL/msg.sh "unknown options! please try --help for more information"
+        $INSTALL_HOME/msg.sh "unknown options! please try ./install.sh --help for more information"
         exit 0
         ;;
     esac
